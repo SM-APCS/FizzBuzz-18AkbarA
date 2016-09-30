9d6a61d9127ed4c5b6d66af81dfec2cbeea8f7d6
Abdullah <Abdullah@AdmiralAkbarsPC>
2016-09-30 13:52:50 -0500

    better than emmets

--- FizzBuzz/src/fizzbuzz/FizzBuzz.java
+++ FizzBuzz/src/fizzbuzz/FizzBuzz.java
@@ -7,8 +7,12 @@ package fizzbuzz;
 
 /**
  *
- * @author Mr. T - CHANGE TO YOUR NAME
+ * @author MC AbsDubs - CHANGE TO YOUR NAME
  * @class AP CSA
+ * 
+ * 
+ * Dab on it
+ * 
  */
 public class FizzBuzz {
 
@@ -17,6 +21,27 @@ public class FizzBuzz {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    	
+    	for (int i = 1; i < 101; i++){
+    		//Check if i is a factor of both 3 and 5
+    		if (i % 3 == 0 && i % 5 == 0){
+    			System.out.println("FizzBuzz");
+    		}
+    		//Else check if i is a factor of only 3
+    		else if (i % 3 == 0){
+    			System.out.println("Fizz");
+    		}
+    		//Else check if i is a factor of only 5
+    		else if (i % 5 == 0){
+    			System.out.println("Buzz");
+    		}
+    		//Else check i is neither a factor 3 nor 5
+    		else{
+    			System.out.println(i);
+    		}
+    	}
     }
+    //Give Emmett an F!
     
+    //ANOTHER EPIC PROGRAM BY ME. YAY!
 }
